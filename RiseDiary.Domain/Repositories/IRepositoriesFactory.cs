137d05bf335eaa5c6d63e56f551a8d8d9dbd49fa
Aleksey-M <malekseyg@gmail.com>
2017-09-25 19:12:58 +0300

    - added repositories factory

---
+++ RiseDiary.Domain/Repositories/IRepositoriesFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace RiseDiary.Domain.Repositories
+{
+    public interface IRepositoriesFactory
+    {
+        IAreasRepository AreasRepository { get; }
+        IRecordTypesRepository RecordTypesRepository { get; }
+        IRecordsRepository RecordsRepository { get; }
+        ICogitationRepository CogitationRepository { get; }
+        IDiaryImagesRepository DiaryImagesRepository { get; }
+    }
+}
