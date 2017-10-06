da6641c3bc3a78585eb5d13561f5940cbbfa4cd8
Aleksey-M <malekseyg@gmail.com>
2017-10-06 23:51:39 +0300

    - added WPF UI project - added Web UI project

---
+++ RiseDiary.UI/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using RiseDiary.Data.SqliteStorages;
+using RiseDiary.Domain.Repositories;
+using RiseDiary.UI.Pages;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Serilog;
+using Serilog.Core;
+
+namespace RiseDiary.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string dataBaseFileName = "DiaryData.db";
+        private readonly Logger _log;
+        private readonly IRepositoriesFactory _repositoriesFactory;
+        private AreasPage _areasPage;
+
+        public AreasPage AreasPage => _areasPage ?? (_areasPage = new AreasPage(_repositoriesFactory));
+        public App()
+        {
+            _log = new LoggerConfiguration().WriteTo.RollingFile("Diary-{Date}.log").CreateLogger();
+            _log.Information("Start app");           
+            try
+            {
+                _repositoriesFactory = new RepositoriesFactory(Environment.CurrentDirectory, dataBaseFileName);                
+            }
+            catch(Exception exc)
+            {
+                _log.Error(exc.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ RiseDiary.UI/Pages/AreasPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using RiseDiary.Domain.Model;
+using RiseDiary.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RiseDiary.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for AreasPage.xaml
+    /// </summary>
+    public partial class AreasPage : Page
+    {
+        private readonly IRepositoriesFactory _factory;
+        private ObservableCollection<DiaryRecordTypeJoined> _areasAndTypes;
+        private ObservableCollection<DiaryArea> _areas;
+        public AreasPage(IRepositoriesFactory factory)
+        {
+            InitializeComponent();
+            DataContext = this;
+            _factory = factory;
+            _areasAndTypes = new ObservableCollection<DiaryRecordTypeJoined>(_factory.RecordTypesRepository.FetchRecordTypesWithAreas().Result);
+            _areas = new ObservableCollection<DiaryArea>(new DiaryArea[] { new DiaryArea { AreaName = "111" } }/*_factory.AreasRepository.FetchAllAreas().Result*/);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {            
+            string areaName = cbArea.Text?.Trim();
+            if (string.IsNullOrWhiteSpace(areaName)) return;
+            string recType = tbRecordType.Text?.Trim();
+            if (string.IsNullOrWhiteSpace(recType)) return;
+
+            int? areaId = _areasAndTypes.FirstOrDefault(a => a.AreaName == areaName)?.AreaId;
+            if (areaId == null)
+            {
+                await _factory.AreasRepository.AddArea(areaName);
+                _areasAndTypes = new ObservableCollection<DiaryRecordTypeJoined>(await _factory.RecordTypesRepository.FetchRecordTypesWithAreas());
+                areaId = _areasAndTypes.FirstOrDefault(a => a.AreaName == areaName)?.AreaId;
+            }
+
+            await _factory.RecordTypesRepository.AddRecordType((int)areaId, recType);
+        }
+    }
+}
