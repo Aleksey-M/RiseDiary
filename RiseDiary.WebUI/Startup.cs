490239d6002661ee2b74fdb8651d8be7e087c7c6
Aleksey-M <malekseyg@gmail.com>
2017-10-24 15:28:58 +0300

    -added areas Page (UI)

--- RiseDiary.WebUI/Startup.cs
+++ RiseDiary.WebUI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RiseDiary.Domain.Repositories;
+using RiseDiary.Data.SqliteStorages;
 
 namespace RiseDiary.WebUI
 {
@@ -22,6 +24,11 @@ namespace RiseDiary.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var fName = Configuration.GetValue<string>("DataBaseFileName");
+            fName = string.IsNullOrWhiteSpace(fName) ? "DefaultName" : fName;
+            var path = Configuration.GetValue<string>("DataBaseFilePath");
+            path = string.IsNullOrWhiteSpace(path) ? Environment.CurrentDirectory : path;
+            services.AddScoped<IRepositoriesFactory, RepositoriesFactory>(sp => new RepositoriesFactory(path, fName));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
