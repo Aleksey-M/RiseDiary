1f1c201e884cdc5bd0abc66ead9ed7a8bab6d437
Aleksey-M <malekseyg@gmail.com>
2019-02-22 10:00:39 +0200

    - some small fixes

--- RiseDiary.WebUI/Pages/DbFile/Index.cshtml.cs
+++ RiseDiary.WebUI/Pages/DbFile/Index.cshtml.cs
@@ -16,6 +16,7 @@ namespace RiseDiary.WebUI.Pages.DbFile
             _config = config;
             _context = context;
         }
+
         public string DataBaseFileName { get; private set; }
         public string DataBaseFileSize { get; private set; }
         public bool MigrationOnStart { get; private set; }
@@ -23,9 +24,11 @@ namespace RiseDiary.WebUI.Pages.DbFile
         public string DataBaseBackupPath { get; private set; }
         public int DataBaseBackupsCount { get; private set; }
         public string DataBaseBackupsSize { get; private set; }
+        public bool IsSqliteUsed { get; private set; }
 
         public void OnGet()
         {
+            IsSqliteUsed = _config.GetValue<int>("usePostgreSql") != 1;
             MigrationOnStart = _config.GetValue<int>("needMigration") > 0;
             BackupOnStart = _config.GetValue<int>("needFileBackup") > 0;
             DataBaseFileName = _config.GetValue<string>("dbFile");
