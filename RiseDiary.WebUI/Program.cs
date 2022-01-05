c192605db6581dd9492d33d4844fed9dc70e781f
Алексей Мамиконян <malekseyg@gmail.com>
2022-01-05 19:17:05 +0300

    конфигурироание порта для локального запуска

--- RiseDiary.WebUI/Program.cs
+++ RiseDiary.WebUI/Program.cs
@@ -10,11 +10,38 @@ namespace RiseDiary.WebUI
             CreateHostBuilder(args).Build().Run();
         }
 
+        private static string? GetCustomPort(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1 && args[0].StartsWith("port="))
+                {
+                    var port = args[0].Split("=")[1].Trim();
+                    if (int.TryParse(port, out int p))
+                    {
+                        return p.ToString();
+                    }
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    
+                    var port = GetCustomPort(args);
+                    if (port != null)
+                    {
+                        webBuilder.UseUrls("http://localhost:" + port + "/");
+                    }                    
                 });
     }
 }
